4aad27d17ce57ac76bac55bf0f1b13320948d6a9
alfredotarigan <43886666+alfredotarigan@users.noreply.github.com>
2019-08-22 12:49:05 +0700

    Update login.c
    Adding username,password and simple database.

--- LoginForm/login.c
+++ LoginForm/login.c
@@ -1,24 +1,33 @@
-#include <stdio.h>k
+#include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
 
 int main(){
-
+    
     char username[25];
+    char password[25];
+
+    // db username & password
+    char user_db[25] = "admin";
+    char password_db[25] = "admin";
+
     int attempt = 0;
+
     bool z = true;
 
     while(z){
-    printf("Masukkan username anda : "); scanf("%s",username);
-    if(strcmp(username,"Alfredo") == 0){
-        printf("Anda berhasil login \n");
+    printf("Enter your username : "); scanf("%s",username);
+    printf("Enter your password : "); scanf("%s",password);
+
+    if(strcmp(username,user_db) == 0 && strcmp(password,password_db) == 0){
+        printf("Login successful \n");
          z = false;
     }
     else{
-        printf("Anda tidak berhasil login! \n");
+        printf("Password not match !! \n");
         attempt++;
         if(attempt == 3){
-            printf("PIN anda telah di blokir,silahkan hubungi ke kantor terdekat.\n");
+            printf("PIN Block! \n");
              z = false;
         }
     }
